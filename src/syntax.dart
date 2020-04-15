2f31131694b5bff6dd14b147e240882278a4f06e
chengfengpiaopiao <869010486@qq.com>
2020-04-15 17:52:41 +0800

    正则保留小数位

--- src/syntax.dart
+++ src/syntax.dart
@@ -10,6 +10,22 @@ void main(){
   print(_deleteTag("asdasda</bar>"));
 
   _testBit();
+
+  double srcDouble = 0.99;
+//  print(srcDouble.toString().indexOf("."));
+//  print(srcDouble.toString().substring(1,4));
+  print(_regxDouble(srcDouble));
+}
+
+
+String _regxDouble(double srcDouble){
+    if(srcDouble.toString().indexOf(".") > 0){
+      //小数位数大于2
+      if(srcDouble.toString().substring(srcDouble.toString().indexOf("."),srcDouble.toString().length).length > 2){
+        return srcDouble.toString().substring(0,srcDouble.toString().indexOf(".") + 3);
+      }
+    }
+    return srcDouble.toStringAsFixed(2);
 }
 
 String _deleteTag(htmlStr){
@@ -22,5 +38,12 @@ String _deleteTag(htmlStr){
 void _testBit(){
   int number = 10;
   print(number<<1); // 20
+  print(number<<1|1); //21
+  print(number<<1&1); //21
   print(number>>1); // 5
+  print(number>>1); //
+
+  number = 4;
+  print(number<<1|1); //9
+  print(number<<1&1); //0
 }
